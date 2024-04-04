ec14c19ea714fc2a7722a41b616262d5c1196be8
DIGIT Pakistan <32742621+digitpk@users.noreply.github.com>
2024-04-04 10:10:48 +0500

    gfgfg

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,3 +1,3 @@
 const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
-const String TMDB_API_KEY = "YOUR_API_KEY";
+const String TMDB_API_KEY = "21442c9c886ac13dc19eb62ad6370512";
 const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
